797061709c0ade4dc3edda9b980560adaafb1c7b
justrandomcoding <justrandomcoding@gmail.com>
2024-11-17 18:19:09 +0000

    working start

---
+++ pages/posts/[id].js
@@ -0,0 +1,36 @@
+import Layout from '../../components/layout';
+import {getAllPostIds, getPostData} from "../../lib/posts";
+
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds();
+    console.log(paths);
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
+export async function getStaticProps({ params }) {
+    console.log(params);
+    const postData = getPostData(params.id);
+    console.log(postData);
+    return {
+        props: {
+            postData,
+        },
+    };
+}
+
+
+export default function Post({ postData }) {
+    return (
+        <Layout>
+            {postData.title}
+            <br />
+            {postData.id}
+            <br />
+            {postData.date}
+        </Layout>
+    );
+}
